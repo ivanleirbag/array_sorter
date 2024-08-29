a20e828b169f349bc8b69fb5246da5b2bf7ded92
ivanleirbag <169113267+ivanleirbag@users.noreply.github.com>
2024-08-29 12:18:08 -0300

    Primer avance Consigna del ejercicio

---
+++ arr_sorter.cpp
@@ -0,0 +1,30 @@
+// array de 1 dimension de 100 elementos int, la cantidad de elem utiles
+//puede variar entre 5 y 100
+//hacer una funcion que ordene los elementos (solo los utiles) de mayor a menor
+//para cargar el array incluir stdlib.h para usar la funcion random(1000)
+//usar randomize() al inicializar la stdlib.h
+//dde mayor a menor
+
+#include <stdlib.h>
+#include <cstdio>
+
+int arr[100];
+
+
+
+void arr_sorter(int *a, int size);
+
+
+
+
+int main(){
+    rando
+
+    return 0;
+}
+
+
+
+
+void arr_sorter(int *a, int size){
+}
