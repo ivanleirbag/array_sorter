9b5399fba2f98a1cd0f180046d06baa8c8e14099
ivanleirbag <169113267+ivanleirbag@users.noreply.github.com>
2024-09-03 10:39:18 -0300

    s

--- arr_sorter.cpp
+++ arr_sorter.cpp
@@ -15,13 +15,21 @@
 using namespace std;
 
 int const arr_size = 100;
+int const rows = 5;
+int const cols = 10;
+
 int arr[arr_size];
+int arr2[rows][cols];
+
 int *pA = &arr[0];
+int *pA2 = &arr2[0][0];
 int N = 1000;
 
 void arr_sorter(int *a, int size);
 
-void print_arr(int *a, int size);
+void print_arr2(int *a, int rows, int cols);
+
+void load_arr2(int *a, int rows, int cols);
 
 int main(){
 
@@ -31,28 +39,64 @@ int main(){
     for (int i = 0; i < arr_size; i++){
         arr[i] = (rand()%N);
     }
+    //------------Llenamos el array con valores aleatorios entre 0 y 100-------------//
+    load_arr2((int *)pA2, rows, cols);
+
     //-----------Contenido del array sin ordenar---------------//
-    printf("                      Array desordenado\n");
-    print_arr(pA, arr_size);
+    printf("                      Array desordenado 1D\n");
+    print_arr2(pA, arr_size, 0);
 
     //-----------Ordena el array------------------//
     arr_sorter(pA, arr_size);
 
     //-----------Contenido del array ordenado---------------/
-    printf("                      Array ordenado\n");
-    print_arr(pA, arr_size);
+    printf("                      Array ordenado 1D\n");
+    print_arr2(pA, 0,arr_size);
+
+    //-----------Contenido del array sin ordenar---------------//
+    printf("                      Array desordenado 2D\n");
+    print_arr2((int *)pA2, rows, cols);
+
+    //-----------Ordena el array------------------//
+    arr_sorter((int *)pA2, (rows*cols));
+
+    //-----------Contenido del array ordenado---------------/
+    printf("                      Array ordenado 2D\n");
+    print_arr2((int *)pA2, rows, cols);
     return 0;
 }
 
-void print_arr(int *a, int size){
-    for (int i = 0; i < size; i++){
-            printf(" %*d", 5, *a);
-            if ((i+1)%10 == 0){
-                printf("\n");
+void print_arr2(int *a, int rows, int cols){
+    int lenght;
+    if (rows != 0 && cols != 0){
+        lenght = rows * cols;
+    }else{
+        lenght = rows + cols;
+    } 
+
+    for (int i = 0; i < lenght; i++){
+            printf("%*d", 5, *a);
+            if (cols != 0){
+                 if ((i+1)%cols == 0){
+                    printf("\n");
+                }
             }
             a++;
         }
         printf("\n");
+}
+
+void load_arr2(int *a, int rows, int cols){
+    int lenght;
+        if (rows != 0 && cols != 0){
+            lenght = rows * cols;
+        }else{
+            lenght = rows + cols;
+        } 
+        for (int i = 0; i < lenght; i++){
+            *(a+i) = rand()%N;
+        }
+        
 }
 
 void arr_sorter(int *a, int size){
